ed87d46f2b128c4023a35e683ba4bfaca1206e4f
--unset <rbk25@RBK25s-Mac-mini.local>
2018-11-20 20:06:39 +0200

    urls in DOM are links

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
+import $ from 'jquery';
+
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: ''
+      term: '',
+      terms: null
     }
   }
 
   onChange (e) {
     this.setState({
-      term: e.target.value
+      term: e.target.value,
     });
   }
 
   search() {
     console.log("you're searching for: ", this.state.term)
     this.props.onSearch(this.state.term);
+    $('#input').val('');
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)}/>       
+      Enter a github username: <input id='input' value={this.state.terms} onChange={this.onChange.bind(this)}/>       
       <button onClick={this.search.bind(this)}> Add Repos </button>
     </div>) 
   }
